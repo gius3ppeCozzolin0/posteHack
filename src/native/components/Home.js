9193c04632458405d4b50549b33fed9f5579160e
gius3ppeCozzolin0 <gius3ppeCozzolin0@gmail.com>
2018-03-10 23:01:05 +0100

    Added vedi tutti

--- src/native/components/Home.js
+++ src/native/components/Home.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
-import { Button, Container, Content, Text } from 'native-base';
-import { Dimensions } from 'react-native';
+import { Container, Content, Text, Button } from 'native-base';
+import { Dimensions, StyleSheet } from 'react-native';
 
 import { Actions } from 'react-native-router-flux';
 import Header from './Header';
 
 import Carousel, { Pagination } from 'react-native-snap-carousel';
 import { View, Image } from 'react-native';
+
+const styles = StyleSheet.create({
+  mainBG: {
+    backgroundColor: '#f1f1f1',
+  },
+  buttonStyle: { alignSelf: 'center' },
+  textButton: {
+    textDecorationLine: 'underline',
+    color: '#0047BB',
+  },
+  sliderText: {
+    marginBottom: 30,
+    textAlign: 'center',
+    color: '#0047BB',
+    fontSize: 21,
+  },
+  dots: {
+    width: 8,
+    height: 8,
+    borderRadius: 5,
+    marginHorizontal: 0,
+    backgroundColor: 'rgba(0, 71, 187, 0.7)',
+  },
+  inactiveDots: {
+    backgroundColor: 'rgba(0, 71, 187, 0.2)',
+  },
+  content: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 48,
+    flex: 1,
+  },
+});
 
 class Home extends Component {
   constructor(props) {
@@ -16,23 +49,23 @@ class Home extends Component {
       entries: [
         {
           title: 'Netflix',
-          illustration: 'https://i.imgur.com/UYiroysl.jpg',
+          illustration: require('../../images/pie.png'),
         },
         {
           title: 'Earlier this morning, NYC',
-          illustration: 'https://i.imgur.com/UPrs1EWl.jpg',
+          illustration: require('../../images/pie.png'),
         },
         {
           title: 'White Pocket Sunset',
-          illustration: 'https://i.imgur.com/MABUbpDl.jpg',
+          illustration: require('../../images/pie.png'),
         },
         {
           title: 'Acrocorinth, Greece',
-          illustration: 'https://i.imgur.com/KZsmUi2l.jpg',
+          illustration: require('../../images/pie.png'),
         },
         {
           title: 'The lone tree, majestic landscape of New Zealand',
-          illustration: 'https://i.imgur.com/2nCt3Sbl.jpg',
+          illustration: require('../../images/pie.png'),
         },
       ],
     };
@@ -48,18 +81,16 @@ class Home extends Component {
   renderItem({ item, index }) {
     const { height, width } = Dimensions.get('window');
     return (
-      <View key={index}>
-        <Text
-          style={{
-            marginBottom: 30,
-            textAlign: 'center',
-            color: '#0047BB',
-            fontSize: 21,
-          }}
-        >
-          {item.title}
-        </Text>
-        <Image style={{ width, height: height - 260 }} source={{ uri: item.illustration }} />
+      <View
+        style={{
+          width,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        key={index}
+      >
+        <Text style={styles.sliderText}>{item.title}</Text>
+        <Image style={{ width: width - 114, height: width - 114 }} source={item.illustration} />
       </View>
     );
   }
@@ -71,18 +102,10 @@ class Home extends Component {
         dotsLength={entries.length}
         activeDotIndex={activeSlide}
         containerStyle={{ backgroundColor: 'transparent' }}
-        dotStyle={{
-          width: 8,
-          height: 8,
-          borderRadius: 5,
-          marginHorizontal: 1,
-          backgroundColor: 'rgba(0, 71, 187, 0.7)',
-        }}
-        inactiveDotStyle={{
-          backgroundColor: 'rgba(0, 71, 187, 0.2)',
-        }}
+        dotStyle={styles.dots}
+        inactiveDotStyle={styles.inactiveDots}
         inactiveDotOpacity={0.4}
-        inactiveDotScale={0.6}
+        inactiveDotScale={1}
         tappableDots={false}
       />
     );
@@ -90,11 +113,10 @@ class Home extends Component {
 
   render() {
     const { height, width } = Dimensions.get('window');
-    console.log(height, 'cjdhf');
     return (
-      <Container>
+      <Container style={styles.mainBG}>
         <Header title="Home" />
-        <Content contentContainerStyle={{ alignItems: 'center' }} padder>
+        <Content contentContainerStyle={styles.content} padder>
           <Carousel
             ref={(c) => {
               this.carousel = c;
@@ -102,14 +124,23 @@ class Home extends Component {
             data={this.state.entries}
             renderItem={this.renderItem}
             sliderWidth={width}
-            siderHeight={height - 260}
+            siderHeight={height - 400}
             itemWidth={width}
-            itemHeight={height - 260}
+            itemHeight={height - 400}
             onSnapToItem={(index) => {
               this.updateCurrentSlide(index);
             }}
           />
           {this.pagination()}
+          <Button
+            transparent
+            onPress={() => {
+              console.log('vedi tutti');
+            }}
+            style={styles.buttonStyle}
+          >
+            <Text style={styles.textButton}>Vedi tutti</Text>
+          </Button>
         </Content>
       </Container>
     );
