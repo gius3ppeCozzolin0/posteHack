3d8ccfe1cf0dc9b2f57ca0edc066e117872b1d9b
Shy <puemos@gmail.com>
2018-03-11 00:34:28 +0100

    Home

--- src/native/components/Home.js
+++ src/native/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Container, Content, Text, View } from 'native-base';
+import { TouchableHighlight } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import Header from './Header';
 
@@ -10,6 +11,14 @@ const Home = () => (
       <Content contentContainerStyle={{ alignItems: 'center', paddingTop: 100 }} padder>
         <Text style={{ fontSize: 56 }}>Ciao Pippo!</Text>
         <Text>Ciao ciao ciao ciao ciao sei circular</Text>
+        <TouchableHighlight
+          style={{
+            width: 300,
+            height: 200,
+          }}
+        >
+          <Text>Wow</Text>
+        </TouchableHighlight>
         <View
           style={{
             flex: 1,
