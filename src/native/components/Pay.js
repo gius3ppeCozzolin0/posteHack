7ce1e949696e9a43361d64b2992ac49eb2915aeb
gius3ppeCozzolin0 <gius3ppeCozzolin0@gmail.com>
2018-03-11 14:20:14 +0100

    paga ora

--- src/native/components/Pay.js
+++ src/native/components/Pay.js
@@ -1,74 +1,96 @@
 import React from 'react';
 import { Button, Container, Content, Text, View } from 'native-base';
+import { Image } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import YellowButton from './YellowButton';
 
-const Pay = () => (
-  <Container
-    style={{
-      backgroundColor: '#f1f1f1',
-    }}
-  >
-    <Content
-      contentContainerStyle={{
-        alignItems: 'center',
-        paddingTop: 40,
-      }}
-      padder
-    >
-      <View
+class Pay extends React.Component {
+  state = {
+    clicked: false,
+  };
+  onPress = () => {
+    if (this.state.clicked) {
+      Actions.home();
+    } else {
+      this.setState({ clicked: true });
+    }
+  };
+  render() {
+    const labelBtn = this.state.clicked ? 'TORNA ALLA HOME' : 'Paga ora';
+    return (
+      <Container
         style={{
-          width: '90%',
-          height: 250,
-          justifyContent: 'center',
-          alignItems: 'center',
-          shadowColor: '#424242',
-          shadowOpacity: 0.2,
-          shadowRadius: 2,
-          overflow: 'hidden',
-          borderRadius: 6,
-          backgroundColor: '#fff',
+          backgroundColor: '#f1f1f1',
         }}
       >
-        <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 40,
-            marginBottom: 20,
-            color: '#0047bb',
+        <Content
+          contentContainerStyle={{
+            alignItems: 'center',
+            paddingTop: 40,
           }}
+          padder
         >
-          Pizzeria Am
-        </Text>
-        <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 16,
-            marginBottom: 50,
-            color: '#0047bb',
-          }}
-        >
-          Corso di Porta Romana, 83 – Milano
-        </Text>
-      </View>
-      <View
-        style={{
-          flex: 1,
-          marginTop: 40,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Text style={{ fontSize: 19, marginBottom: 20 }}>Transazione effettuata</Text>
+          <View
+            style={{
+              width: '90%',
+              height: 250,
+              justifyContent: 'center',
+              alignItems: 'center',
+              shadowColor: '#424242',
+              shadowOpacity: 0.2,
+              shadowRadius: 2,
+              overflow: 'hidden',
+              borderRadius: 6,
+              backgroundColor: '#fff',
+            }}
+          >
+            <Text
+              style={{
+                textAlign: 'center',
+                fontSize: 40,
+                marginBottom: 20,
+                color: '#0047bb',
+              }}
+            >
+              Pizzeria Am
+            </Text>
+            <Text
+              style={{
+                textAlign: 'center',
+                fontSize: 16,
+                marginBottom: 50,
+                color: '#0047bb',
+              }}
+            >
+              Corso di Porta Romana, 83 – Milano
+            </Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              marginTop: 40,
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Text style={{ fontSize: 19, marginBottom: 20 }}>Transazione effettuata</Text>
 
-        <Text style={{ fontSize: 40, marginBottom: 50, color: '#0047bb' }}>€ 125,60</Text>
-      </View>
-      <View style={{}}>
-        <YellowButton label="TORNA ALLA HOME" onPress={Actions.home} />
-      </View>
-    </Content>
-  </Container>
-);
+            <Text style={{ fontSize: 40, marginBottom: 50, color: '#0047bb' }}>€ 125,60</Text>
+
+            {this.state.clicked ? (
+              <Image style={{ width: 54, height: 54 }} source={require('../../images/ok.png')} />
+            ) : (
+              <View style={{ width: 54, height: 54 }} />
+            )}
+          </View>
+          <View style={{}}>
+            <YellowButton label={labelBtn} onPress={this.onPress} />
+          </View>
+        </Content>
+      </Container>
+    );
+  }
+}
 
 export default Pay;
