8c067bdf7714797501df4ac7d3e6603e29eac81a
gius3ppeCozzolin0 <gius3ppeCozzolin0@gmail.com>
2018-03-11 04:30:58 +0100

    groups

--- src/native/containers/Groups.js
+++ src/native/containers/Groups.js
@@ -3,14 +3,20 @@ import { connect } from 'react-redux';
 
 import groupsActions from '../actions/groups';
 
-const GroupsContainer = ({ Layout, groups, match }) => <Layout groups={groups} match={match} />;
+const GroupsContainer = ({
+  Layout, groups, contacts, match,
+}) => (
+  <Layout contacts={contacts} groups={groups} match={match} />
+);
 
 const mapStateToProps = (state) => {
   const groups = { ...state.groups.items };
+  const contacts = { ...state.contacts.items };
   const groupsOrder = [...state.groups.order];
 
   return {
     groups: groupsOrder.map(groupId => groups[groupId]),
+    contacts,
   };
 };
 
