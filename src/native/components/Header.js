d484d99a73c5d69f389a30c8876faee04823cc2d
gius3ppeCozzolin0 <gius3ppeCozzolin0@gmail.com>
2018-03-11 02:38:34 +0100

    header

--- src/native/components/Header.js
+++ src/native/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Body, Button, Header, Icon, Left, Right, Title } from 'native-base';
+import { Body, Button, Header, Icon, Left, Right, Text, Title } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 
 const PosteHeader = ({ title, showBack }) => (
-  <Header style={{ backgroundColor: 'white', borderBottomColor: 'rgb(224,224,224)' }}>
+  <Header
+    iosBarStyle="dark-content"
+    style={{ backgroundColor: 'white', borderBottomColor: 'rgb(224,224,224)' }}
+  >
     <Left>
       {showBack && (
         <Button transparent onPress={() => Actions.pop()}>
@@ -13,7 +16,7 @@ const PosteHeader = ({ title, showBack }) => (
       )}
     </Left>
     <Body>
-      <Title style={{ color: 'rgb(66,66,66)', fontSize: 16 }}>{title}</Title>
+      <Title style={{ color: 'rgb(66,66,66)', fontSize: 16, overflow: 'visible' }}>{title}</Title>
     </Body>
     <Right />
   </Header>
