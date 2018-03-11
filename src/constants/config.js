afe7f68f5042e177508b6dffe73f7e96d371c964
gius3ppeCozzolin0 <gius3ppeCozzolin0@gmail.com>
2018-03-11 12:59:38 +0100

    Added images in analytics

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,11 +1,11 @@
-const devMode = (process.env.NODE_ENV !== 'development');
+const devMode = process.env.NODE_ENV !== 'development';
 
 export default {
   // App Details
   appName: 'PosteHack',
-  home: "Home",
-  groups: "Guppi",
-  analytics: "Analytics",
+  home: 'Home',
+  groups: 'Guppi',
+  analytics: 'Statistiche',
 
   // Build Configuration - eg. Debug or Release?
   DEV: devMode,
--- src/native/components/Analytics.js
+++ src/native/components/Analytics.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { Container, Content, Text } from 'native-base';
+import { Container, Content } from 'native-base';
+import { Image, Dimensions } from 'react-native';
+import Header from './Header';
+
+const { width } = Dimensions.get('window');
 
 const Analytics = () => (
   <Container>
-    <Content padder>
-      <Text>Analytics</Text>
+    <Header title="Statistiche" />
+    <Content contentContainerStyle={{ alignItems: 'center', justifyContent: 'center' }}>
+      <Image
+        resizeMode="contain"
+        style={{ width: width - 30 }}
+        source={require('../../images/card_graph.png')}
+      />
+      {/* <Image
+        resizeMode="contain"
+        style={{ width: width - 30, paddingBottom: 30 }}
+        source={require('../../images/card_graph1.png')}
+      />
+      <Image
+        resizeMode="contain"
+        style={{ width: width - 30, paddingBottom: 30 }}
+        source={require('../../images/card_graph2.png')}
+      /> */}
     </Content>
   </Container>
 );
