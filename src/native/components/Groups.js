dc6ec3a1623be4dbb892659ac964e3831a9a4bbe
Luca Galeazzi <luca@igenius.net>
2018-03-11 01:04:54 +0100

    fixed groups style

--- src/native/components/Groups.js
+++ src/native/components/Groups.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import { Body, Container, Content, Left, List, ListItem, Right, Text, Thumbnail } from 'native-base';
 import Header from './Header';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  mainBG: {
+    backgroundColor: '#f1f1f1',
+  },
+});
 
 const Groups = ({ groups }) => (
-  <Container>
+  <Container style={styles.mainBG}>
     <Header title="Gruppi" />
-    <Content padder>
+    <Content style={{ paddingTop: 20 }}>
       <List
+        style={{ backgroundColor: 'white' }}
         dataArray={groups}
-        renderRow={({ name, expense, contacts }) => (
-          <ListItem avatar>
-            <Left>
-              <Thumbnail small source={require('../../images/home_black.png')} />
-            </Left>
-            <Body>
-              <Text>{name}</Text>
-              <Text note>{`${contacts.length} partecipanti`}</Text>
-            </Body>
-            <Right>
-              <Text note>{expense}</Text>
-            </Right>
-          </ListItem>
+        renderRow={({
+ name, expense, contacts, image,
+}) => (
+  <ListItem avatar>
+    <Left>
+      <Thumbnail source={image} />
+    </Left>
+    <Body style={{ paddingTop: 16, paddingBottom: 16 }} >
+      <Text style={{ paddingBottom: 8 }}>{name}</Text>
+      <Text note>{`${contacts.length} partecipanti`}</Text>
+    </Body>
+    <Right style={{ paddingTop: 16, paddingBottom: 16 }} >
+      <Text note>{expense}</Text>
+    </Right>
+  </ListItem>
           )}
       />
     </Content>
