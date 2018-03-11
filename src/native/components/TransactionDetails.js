c640ecea0ed6425554c65c7c3cd15f397e189dcd
gius3ppeCozzolin0 <gius3ppeCozzolin0@gmail.com>
2018-03-11 09:56:37 +0100

    user transaction card details

--- src/native/components/TransactionDetails.js
+++ src/native/components/TransactionDetails.js
@@ -8,11 +8,17 @@ const styles = StyleSheet.create({
   mainBG: {
     backgroundColor: '#f1f1f1',
   },
+  parentItemRow: {
+    paddingLeft: 16,
+    paddingRight: 16,
+    backgroundColor: '#fff',
+  },
   itemRow: {
     backgroundColor: 'white',
     flexDirection: 'row',
     alignItems: 'center',
-    padding: 16,
+    paddingTop: 16,
+    paddingBottom: 16,
     borderBottomColor: 'rgb(224,224,224)',
     borderBottomWidth: 1,
   },
@@ -35,6 +41,8 @@ class TransactionDetails extends Component {
     };
 
     this.renderDescription = this.renderDescription.bind(this);
+    this.renderCardInfo = this.renderCardInfo.bind(this);
+
     this.renderUserTransactions = this.renderUserTransactions.bind(this);
     this.renderUserTransaction = this.renderUserTransaction.bind(this);
   }
@@ -44,22 +52,24 @@ class TransactionDetails extends Component {
 
     return (
       <View>
-        <View style={{
-        backgroundColor: '#EEDD00',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingTop: 24,
-        paddingBottom: 24,
-}}
+        <View
+          style={{
+            backgroundColor: '#EEDD00',
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingTop: 24,
+            paddingBottom: 24,
+          }}
         >
           <Thumbnail source={transaction.image} />
-          <View style={{
-paddingTop: 16,
-flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-}}
+          <View
+            style={{
+              paddingTop: 16,
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
           >
             <Text style={{ fontSize: 18 }}>{`Hai pagato ${transaction.quantity}`}</Text>
             <Text style={{ fontSize: 18 }}>{`a ${expense.recipient}`}</Text>
@@ -74,15 +84,60 @@ flex: 1,
     const { userTransactions, contacts } = this.props;
     const userTransaction = userTransactions[userTransactionId];
     const contact = contacts[userTransaction.userId];
-
     return (
-      <View key={userTransactions.id} style={styles.itemRow} avatar >
-        <View>
-          <Text style={{ paddingBottom: 8 }}>{`${contact.name} ${contact.lastname}`}</Text>
-          {
-            userTransaction.failed ?
-              <Text style={{ flex: 1 }}>Non pagato</Text> : <Text>Pagato</Text>
-          }
+      <View style={styles.parentItemRow} key={userTransactionId}>
+        <View style={styles.itemRow} avatar>
+          <Text style={{ flex: 1, paddingTop: 10, paddingBottom: 4 }}>
+            {`${contact.name} ${contact.lastname}`}
+          </Text>
+          {userTransaction.failed ? (
+            <Text style={{ flex: 1, textAlign: 'right', color: 'rgb(255,44,81)' }}>Non pagato</Text>
+          ) : (
+            <Text style={{ flex: 1, textAlign: 'right', color: 'rgb(0,200,83)' }}>Pagato</Text>
+          )}
+        </View>
+      </View>
+    );
+  }
+
+  renderCardInfo() {
+    const {
+      transaction: {
+        card, date, quantity, category,
+      },
+    } = this.state;
+    const CardNumbers = card.split(' ')[card.split(' ').length - 1];
+    return (
+      <View style={styles.parentItemRow}>
+        <View style={styles.itemRow}>
+          <View style={{ flex: 1 }}>
+            <Text style={styles.note}>Con</Text>
+            <Text style={{ paddingTop: 8 }}>{`Carta(${CardNumbers})`}</Text>
+          </View>
+          <View style={{ flex: 1, alignSelf: 'flex-end' }}>
+            <Text
+              style={{
+                flex: 1,
+                textAlign: 'right',
+                paddingTop: 28,
+                alignSelf: 'flex-end',
+              }}
+            >
+              {quantity}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.itemRow}>
+          <View>
+            <Text style={styles.note}>Tipo di transazione</Text>
+            <Text style={{ paddingTop: 8 }}>{category}</Text>
+          </View>
+        </View>
+        <View style={styles.itemRow}>
+          <View>
+            <Text style={styles.note}>Quando</Text>
+            <Text style={{ paddingTop: 8 }}>{date}</Text>
+          </View>
         </View>
       </View>
     );
@@ -92,7 +147,19 @@ flex: 1,
     const { transaction } = this.state;
 
     return (
-      <View style={{ paddingTop: 20 }}>
+      <View>
+        <Text
+          style={{
+            color: 'rgb(66, 66, 66)',
+            fontSize: 13,
+            padding: 16,
+            paddingTop: 24,
+            paddingBottom: 20,
+          }}
+        >
+          Dettagli pagamento
+        </Text>
+
         {transaction.userTransactions.map(this.renderUserTransaction)}
       </View>
     );
@@ -106,6 +173,7 @@ flex: 1,
         <Header title={transaction.title} showBack />
         <Content>
           {this.renderDescription()}
+          {this.renderCardInfo()}
           {this.renderUserTransactions()}
         </Content>
       </Container>
